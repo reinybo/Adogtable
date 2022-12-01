914d470d434bf8a7b792928ae7deb83cb77b9d69
Reiny Botros <reinybotros@Reinys-MacBook-Pro.local>
2022-12-01 15:51:38 -0800

    pictures, ids, names, locations retrieved

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const URL = "https://api.rescuegroups.org/v5/public/";
 // const URL = "https://api.rescuegroups.org/v5/public/animals/breeds";
 
 const $dogSection = $('#dogs-section');
+const $dogSection2 = $('#dogs-section2');
 let dogsObjectsArray;
 
 
@@ -13,14 +14,19 @@ function handleGetData() {
     $.ajax({url: URL + "animals/search/available/", headers: {Authorization: apiKey}}).then(( data ) => {
 
         console.log('sucess');
-        console.log("data: " + data );
+        console.log("data: ");
         console.log(data);
-        console.log("data.data[0].attributes.name: " + data.data[0].attributes.name)
-        dogsObjectsArray=data.data;
-        console.log("data.data: " + data.data);
-        console.log('data.data[0]:')
-        console.log(data.data[0])
-        render(data.data);
+        console.log("data.data[0].attributes.name: ");
+        console.log(data.data[0].attributes.name);
+        console.log(data.included[6].attributes.city);
+        dogsDataArray=data.data;
+        dogsIncludesArray=data.included;
+        console.log(data.data[0].relationships.locations);
+        console.log('data.data[0]:');
+        console.log(data.data[0]);
+ //       render(data.included);
+//        render2(data.data)
+        render();
     }, ( error ) => {
         console.log( 'bad request', error );
     } )
@@ -28,16 +34,65 @@ function handleGetData() {
 
 
 
-function render(dogs) {
-    const availDogs = dogs.map(function(dogObject, index) {
+function render() {
+    const availDogs = dogsDataArray.map(function(dogObject, index) {
         return `
         <article data-index="${index}">
-            <h2>${dogObject.attributes.name}</h2>      
-            <img id='dogImg' src="${dogObject.attributes.trackerimageUrl}"/>
+            <h2>${dogObject.attributes.name}</h2> 
+            <h3>${dogObject.id}</h3>
+            <img id='dogImg' src="${dogObject.attributes.pictureThumbnailUrl}"/>
         </article>
         `;
     })
     $dogSection.html(availDogs);
+    
+    const availDogs2 = dogsIncludesArray.map(function(dogObject, index) {
+        if (dogObject.type === 'locations'){
+            console.log("found loc");
+            return `
+            <article data-index="${index}">
+                <h3>${dogObject.attributes.coordinates}</h3>
+                <h4>${dogObject.id}</h4>
+            </article>
+            `;
+        }
+    })
+    $dogSection2.html(availDogs2);
+    
 }
+
+
+
+// function render(doggies) {
+    // console.log('running render');
+    // const availDogs2 = doggies.map(function(dogObject, index) {
+    //     if (dogObject.type === 'locations'){
+    //         console.log("found loc");
+    //         return `
+    //         <article data-index="${index}">
+    //             <h3>${dogObject.attributes.coordinates}</h3>
+    //             <h4>${dogObject.id}</h4>
+    //         </article>
+    //         `;
+    //     }
+    // })
+    // $dogSection.html(availDogs2);
+// }
+
+
+
+// function render2(dogs) {
+//     const availDogs = dogs.map(function(dogObject, index) {
+//         render(dogs)
+//         return `
+//         <article data-index="${index}">
+//             <h2>${dogObject.attributes.name}</h2> 
+//             <h3>${dogObject.id}</h3>
+//             <img id='dogImg' src="${dogObject.attributes.pictureThumbnailUrl}"/>
+//         </article>
+//         `;
+//     })
+//     $dogSection2.html(availDogs);
+// }
 
 handleGetData();
