a718a44b01c67ca2fbccc30afe67180c4acab368
Reiny Botros <reinybotros@Reinys-MacBook-Pro.local>
2022-12-06 20:03:33 -0800

    distance and layout

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const URL = "https://api.rescuegroups.org/v5/public/";
+
 // const URL = "https://api.rescuegroups.org/v5/public/animals/breeds";
 
 const $dogSection = $('#dogs-section');
@@ -8,58 +8,150 @@ let dogsObjectsArray;
 
 const apiKey = "lIjLEOom";
 
+const URL = "https://api.rescuegroups.org/v5";
+
+const $form = $('form');
+const $zipInputSpace = $('#zip');
+const $radiusInputSpace = $('#radius');
+
+
+
+
+$form.on('submit', handleGetData)
+
+$dogSection.on('click', handleClick);
+
 
 function handleGetData() {
+    const zipcodeInput = $zipInputSpace.val();
+    const radiusInput = $radiusInputSpace.val(); 
 
-    $.ajax({url: URL + "animals/search/available/", headers: {Authorization: apiKey}}).then(( data ) => {
+    $.ajax({type: 'POST', 
+            url: URL + "/public/animals/search/available/dogs", 
+            headers: {Authorization: apiKey}, 
+            data: JSON.stringify({
+                     data: {
+                         "filterRadius": {
+                             "miles": radiusInput,
+                             "postalcode": zipcodeInput,
+                            }
+                     }
+             })}).then((data) => {
 
-        console.log('sucess');
+        console.log('success');
         console.log("data: ");
         console.log(data);
-        console.log("data.data[0].attributes.name: ");
-        console.log(data.data[0].attributes.name);
-        console.log(data.included[6].attributes.city);
         dogsDataArray=data.data;
         dogsIncludesArray=data.included;
-        console.log(data.data[0].relationships.locations);
-        console.log('data.data[0]:');
-        console.log(data.data[0]);
- //       render(data.included);
-//        render2(data.data)
         render();
+
     }, ( error ) => {
         console.log( 'bad request', error );
     } )
 }
 
 
-
 function render() {
     const availDogs = dogsDataArray.map(function(dogObject, index) {
+
         return `
-        <article data-index="${index}">
-            <h2>${dogObject.attributes.name}</h2> 
-            <h3>${dogObject.id}</h3>
-            <img id='dogImg' src="${dogObject.attributes.pictureThumbnailUrl}"/>
-        </article>
+            <article data-index="${index}">
+                <h2>${dogObject.attributes.name}</h2> 
+                <h5>${dogObject.attributes.distance} miles</h5>
+                <img id='dogImg' src="${dogObject.attributes.pictureThumbnailUrl}"/>
+            </article>
         `;
     })
     $dogSection.html(availDogs);
-    
-    const availDogs2 = dogsIncludesArray.map(function(dogObject, index) {
-        if (dogObject.type === 'locations'){
-            console.log("found loc");
-            return `
-            <article data-index="${index}">
-                <h3>${dogObject.attributes.coordinates}</h3>
-                <h4>${dogObject.id}</h4>
-            </article>
-            `;
-        }
-    })
-    $dogSection2.html(availDogs2);
-    
 }
+
+
+
+function handleClick() {
+    const $target = $(this);
+    // const answer = $target.text();
+    const $dogClicked = $target.closest('article');
+    console.log($dogClicked);
+    
+    // const dogIndex = $dogClicked.attr('data-index');
+    // console.log($dogClicked.attr(data-index));
+    // const questionReference = questionObjectsArray[questionIndex];
+    
+    // if (questionReference.correct_answer === answer) {
+    //     $answerResult.text('You Guessed Correctly');
+    // } else {
+    //     $answerResult.text('You Guessed Incorrectly');
+    // }
+}
+
+
+
+
+
+     //   console.log("data.data[0].attributes.name: ");
+     //   console.log(data.data[0].attributes.name);
+     //   console.log(data.included[6].attributes.city);
+     //   dogsDataArray=data.data;
+     //   dogsIncludesArray=data.included;
+     //   console.log(data.data[0].relationships.locations);
+     //   console.log('data.data[0]:');
+     //   console.log(data.data[0]);
+ //       render(data.included);
+//        render2(data.data)
+ //       render();
+
+
+
+
+// function handleSubmission() {
+//     console.log('button pressed');
+//     const zipcodeInput = $zipInputSpace.val();
+//     console.log($zipInputSpace.val());
+//     // console.log($(radiusInputSpace).val());
+//     // const radiusInput = radiusInputSpace.value; 
+//     return zipcodeInput;   
+// }
+
+
+
+
+
+
+
+    
+//     const availDogs2 = dogsIncludesArray.map(function(dogObject, index) {
+//         if (dogObject.type === 'locations'){
+//             console.log("found loc");
+//             let locDogId = dogObject.id;
+            
+//             const availDogs = dogsDataArray.map(function(dogObject, index) {
+//                 if (dogObject.relationships.locations.data[0].id === locDogId) {
+//                     console.log('found match');
+//                     let doggyName = dogObject.attributes.name;
+//                     return `
+//                         <article data-index="${index}">
+//                          <h2>${dogObject.attributes.name}</h2> 
+//                          <h3>${dogObject.id}</h3>
+//                          <img id='dogImg' src="${dogObject.attributes.pictureThumbnailUrl}"/>
+//                         </article>                    
+//                     `;
+//                 }
+//             }).join('');
+//             $dogSection.html(availDogs);
+//             return `
+//             <article data-index="${index}">
+//                 <h2>${dogObject.attributes.name}</h2> 
+//                 <h3>${dogObject.id}</h3>
+//                 <img id='dogImg' src="${dogObject.attributes.pictureThumbnailUrl}"/>
+//                 <h3>${dogObject.attributes.coordinates}</h3>
+//                 <h4>${dogObject.id}</h4>
+//             </article>
+//             `;
+//         }
+//     }).join('');
+//     $dogSection2.html(availDogs2);
+    
+// }
 
 
 
@@ -94,5 +186,3 @@ function render() {
 //     })
 //     $dogSection2.html(availDogs);
 // }
-
-handleGetData();
